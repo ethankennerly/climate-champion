6d5893f205c2c3225dce4542aae1532b527bf5bd
Ethan Kennerly <ethankennerly@gmail.com>
2019-03-03 18:07:51 -0800

    Tilemap Mixer: Drafts finding shuffled tiles.

--- ClimateChampion/Assets/Scripts/TilemapMixer.cs
+++ ClimateChampion/Assets/Scripts/TilemapMixer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -15,11 +16,14 @@ namespace FineGameDesign.Utils
     public sealed class TilemapMixer : MonoBehaviour
     {
         [Serializable]
-        public struct MixInput
+        public class MixInput
         {
             public float lowerBound;
+
             [Header("Hides tile if empty.")]
             public Tilemap map;
+
+            public List<TileBase> tilesToMix;
         }
 
         [SerializeField]
@@ -27,6 +31,8 @@ namespace FineGameDesign.Utils
 
         [SerializeField]
         private MixInput[] m_MixInputs;
+
+        private bool m_Initialized;
 
         [SerializeField]
         private float m_Mix;
@@ -47,9 +53,45 @@ namespace FineGameDesign.Utils
             SetMix(m_Mix);
         }
 
+        private void OnEnable()
+        {
+            Init();
+        }
+
         private void SetMix(float nextMix)
         {
+            Init();
             m_Mix = nextMix;
+        }
+
+        private void Init()
+        {
+            if (m_Initialized)
+                return;
+            m_Initialized = true;
+
+            foreach (MixInput input in m_MixInputs)
+            {
+                input.tilesToMix = GetTiles(input.map);
+                Deck.ShuffleList(input.tilesToMix);
+            }
+        }
+
+        private List<TileBase> GetTiles(Tilemap map)
+        {
+            List<TileBase> tiles = new List<TileBase>();
+            if (map == null)
+                return tiles;
+
+            BoundsInt bounds = map.cellBounds;
+            foreach (Vector3Int cell in bounds.allPositionsWithin)
+            {
+                if (!map.HasTile(cell))
+                    continue;
+
+                tiles.Add(map.GetTile(cell));
+            }
+            return tiles;
         }
     }
 }
